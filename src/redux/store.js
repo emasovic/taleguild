96b89c00315adfaae42500bc91f4549aefac6048
eko <elvismasovic@spinviewglobal.com>
2020-05-07 16:53:20 +0200

    store changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,12 @@
-import {configureStore, applyMiddleware} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 import user from './user';
 import stories from './story';
 import toast from './toast';
 
-const hehe = console.log('hehe');
-export default configureStore(
-	{
-		reducer: {
-			user,
-			stories,
-			toast,
-		},
+export default configureStore({
+	reducer: {
+		user,
+		stories,
+		toast,
 	},
-	applyMiddleware(hehe)
-);
+});
