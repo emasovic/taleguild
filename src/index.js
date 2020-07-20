cf0fb2d872982fdcf708903040857c3a8a09ca59
eko <elvismasovic@spinviewglobal.com>
2020-07-20 16:50:58 +0200

    added react-snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {hydrate, render} from 'react-dom';
 import {Provider} from 'react-redux';
 
 import store from './redux/store';
@@ -16,15 +16,29 @@ import './index.scss';
 
 store.dispatch(getUser());
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-			<Toasts />
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+	hydrate(
+		<React.StrictMode>
+			<Provider store={store}>
+				<App />
+				<Toasts />
+			</Provider>
+		</React.StrictMode>,
+		rootElement
+	);
+} else {
+	render(
+		<React.StrictMode>
+			<Provider store={store}>
+				<App />
+				<Toasts />
+			</Provider>
+		</React.StrictMode>,
+		rootElement
+	);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
