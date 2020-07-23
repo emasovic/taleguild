844525e6eabdc345070baefa4b337bcfaf0e6785
eko <elvismasovic@spinviewglobal.com>
2020-07-23 13:12:51 +0200

    [TextEditor]: Fixed bug with editor selection

--- src/components/widgets/text-editor/TextEditor.js
+++ src/components/widgets/text-editor/TextEditor.js
@@ -3,6 +3,8 @@ import {Slate, Editable, withReact} from 'slate-react';
 import {createEditor} from 'slate';
 import {withHistory} from 'slate-history';
 
+import {usePrevious} from 'hooks/compare';
+
 import HoveringToolbar from './widgets/HoveringToolbar';
 import Element from './widgets/Element';
 import Leaf from './widgets/Leaf';
@@ -11,10 +13,16 @@ import './TextEditor.scss';
 
 const CLASS = 'st-TextEditor';
 
-export default function TextEditor({value, onChange, onKeyDown}) {
+export default function TextEditor({value, onChange, onKeyDown, pageId}) {
 	const renderElement = useCallback(props => <Element {...props} />, []);
 	const renderLeaf = useCallback(props => <Leaf {...props} />, []);
 	const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+	const previousId = usePrevious(pageId);
+
+	if (pageId !== previousId && editor.selection) {
+		const point = {path: [0, 0], offset: 0};
+		editor.selection = {anchor: point, focus: point};
+	}
 
 	return (
 		<Slate editor={editor} value={value} onChange={onChange}>
