bcbeeac3ccf0a8af2df5708120683e00835a55d5
eko <elvismasovic@spinviewglobal.com>
2021-01-18 14:55:59 +0100

    [server]: home route fixed meta tags issue, added default story image

--- server.js
+++ server.js
@@ -16,10 +16,9 @@ const getIdFromSlug = slug => {
 const PORT = process.env.PORT || 5000;
 
 const DEFAULT_IMAGE_URL = `${env.PUBLIC_URL}/taleguild-share.png`;
+const DEFAULT_STORY_IMAGE_URL = `${env.PUBLIC_URL}/default-story-share.png`;
 
 const app = express();
-
-app.use(express.static(path.resolve(__dirname, 'build')));
 
 const filePath = path.resolve(__dirname, 'build', 'index.html');
 
@@ -40,9 +39,9 @@ app.get('/story/:slug', async (req, res) => {
 			image =
 				image && image.upload_file_id
 					? env.API_URL + image.upload_file_id.url
-					: DEFAULT_IMAGE_URL;
+					: DEFAULT_STORY_IMAGE_URL;
 		} catch (error) {
-			image = DEFAULT_IMAGE_URL;
+			image = DEFAULT_STORY_IMAGE_URL;
 		}
 
 		story = story.toJSON();
@@ -106,6 +105,26 @@ app.get('/user/:username', async (req, res) => {
 		res.sendFile(filePath);
 	}
 });
+
+app.get('/', function(req, res) {
+	fs.readFile(filePath, 'utf8', (err, data) => {
+		if (err) {
+			return console.log(err);
+		}
+
+		data = data
+			.replace(
+				/__TITLE__/g,
+				'Taleguild | Social Network for Short Stories Writers & Essayists'
+			)
+			.replace(/__DESCRIPTION__/g, 'Social Network of Short Story Writers & Essayists')
+			.replace(/__IMAGE_URL__/g, DEFAULT_IMAGE_URL);
+
+		res.send(data);
+	});
+});
+
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('/*', function(req, res) {
 	fs.readFile(filePath, 'utf8', (err, data) => {
