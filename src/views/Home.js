c7154307b50d9f7d19ed389cc56698a47a3aceb0
eko <elvismasovic@spinviewglobal.com>
2021-06-15 16:04:42 +0200

    [Home]: refactored logic

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,39 +1,11 @@
-import React, {useEffect} from 'react';
-import {shallowEqual, useSelector, useDispatch} from 'react-redux';
+import React from 'react';
 
 import {DEFAULT_CRITERIA} from 'types/story';
-
-import {selectUserId} from 'redux/user';
-import {selectFollowing, loadFollowing} from 'redux/following';
-
-import Loader from 'components/widgets/loader/Loader';
 
 import Explore from './Explore';
 
 export default function Home() {
-	const dispatch = useDispatch();
-	const {userId, following, loading} = useSelector(state => {
-		const userId = selectUserId(state);
-		return {
-			following: selectFollowing(state, userId),
-			loading: state.following.loading,
-			userId,
-		};
-	}, shallowEqual);
-
-	useEffect(() => {
-		if (userId) {
-			dispatch(loadFollowing({follower: userId}));
-		}
-	}, [dispatch, userId]);
-
-	if (!userId || loading || !following) {
-		return <Loader />;
-	}
-
-	const userIds = following && following.length ? following.map(item => item.user.id) : [];
-
-	const criteria = {...DEFAULT_CRITERIA, user_in: [...userIds, userId]};
+	const criteria = {...DEFAULT_CRITERIA, relevant: true};
 
 	return <Explore criteria={criteria} />;
 }
